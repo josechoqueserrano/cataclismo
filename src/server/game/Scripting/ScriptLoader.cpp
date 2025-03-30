c19d37a6d726c42c3267a31e9436ea2597d41ae4
unknown <choquejose@tigo.net.bo>
2025-03-30 17:34:52 -0400

    Se agrego a ScriptLoader.cpp el metodo y la llamada al metodo AddSC_npc_sister_gold_skimmer();

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1462,13 +1462,13 @@ void AddBattlegroundScripts()
 
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
-void AddSC_npc_anubisath_sentinel();
+void AddSC_npc_sister_gold_skimmer();
 #endif
 
 void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
-
+    AddSC_npc_sister_gold_skimmer();
 #endif
 }
